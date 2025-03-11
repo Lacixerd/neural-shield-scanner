7f1aa16c69152bcca5b3d6034b820870a14ede2c
Cem Erdem <cemerdem2006@gmail.com>
2025-03-11 15:05:50 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ import json
 import requests
 import sys
 import os
+import argparse
 
 if getattr(sys, 'frozen', False):
     BASE_DIR = os.path.dirname(sys.executable)
@@ -55,36 +56,63 @@ def run_port_scanner():
 
 if __name__ == "__main__":
     try:
-        if config['license_key'] == "":
-            print("License key not found in config.json")
-            exit()
-        url = config['api_url'] + "authorize-license-key/"
-        headers = {
-            "Authorization": f"Token {config['api_token']}",
-            "Content-Type": "application/json"
-        }
-        payload = {
-            "license_key": config['license_key']
-        }
-        try:
-            response = requests.post(url, headers=headers, json=payload)
-            if response.status_code == 200:
-                print("License key authorized successfully.")
-            else:
-                print(f"License key authorization failed: {response.status_code}\nError: {response.text}")
+        parser = argparse.ArgumentParser(description='Network Security System For Neural Shield')
+        parser.add_argument('--no-sniffer', action='store_true', help='Disable packet sniffer (Not recommended)')
+        parser.add_argument('--no-ids', action='store_true', help='Disable intrusion detection system (Not recommended)')
+        parser.add_argument('--no-scanner', action='store_true', help='Disable port scanner (Not recommended)')
+        parser.add_argument('--license-key', type=str, help='License key to authorize')
+        args = parser.parse_args()
+        if not args.license_key:
+            if config['license_key'] == "":
+                print("Please enter a license key to authorize. Use \"--license-key <key>\" to authorize.")
                 exit()
-        except Exception as e:
-            print(f"License key authorization error: {e}")
-            exit()
-        # sniffer_thread = Thread(target=run_packet_sniffer)
-        # sniffer_thread.daemon = True
-        # sniffer_thread.start()
+        else:
+            with open(config_path, 'w') as f:
+                config['license_key'] = args.license_key
+                json.dump(config, f, indent=2)
+            url = config['api_url'] + "authorize-license-key/"
+            headers = {
+                "Authorization": f"Token {config['api_token']}",
+                "Content-Type": "application/json"
+            }
+            payload = {
+                "license_key": config['license_key']
+            }
+            try:
+                response = requests.post(url, headers=headers, json=payload)
+                if response.status_code == 200:
+                    print("License key authorized successfully.")
+                else:
+                    print(f"License key authorization failed: {response.status_code}\nError: {response.text}\nCheck your subscription status at https://neuralshieldai.com/.")
+                    with open(config_path, 'w') as f:
+                        config['license_key'] = ""
+                        json.dump(config, f, indent=2)
+                    print("Exiting...")
+                    exit()
+            except Exception as e:
+                print(f"License key authorization error: {e}")
+                print("Exiting...")
+                exit()
 
-        ids_thread = Thread(target=run_ids)
-        ids_thread.daemon = True
-        ids_thread.start()
+        if not args.no_sniffer:
+            sniffer_thread = Thread(target=run_packet_sniffer)
+            sniffer_thread.daemon = True
+            sniffer_thread.start()
+        else:
+            print("Packet sniffer disabled.")
 
-        run_port_scanner()
+        if not args.no_ids:
+            ids_thread = Thread(target=run_ids)
+            ids_thread.daemon = True
+            ids_thread.start()
+        else:
+            print("Intrusion detection system disabled.")
+
+        if not args.no_scanner:
+            run_port_scanner()
+        else:
+            print("Port scanner disabled.")
+
     except KeyboardInterrupt:
         print("\nMain program stopping...")
     except Exception as e:
